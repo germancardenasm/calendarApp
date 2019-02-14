b80073f9c7cd933ccd12a048387dbd49abf06c05
German Cardenas <gcardenas@globalk.local>
2019-02-14 18:20:20 -0500

    renaming

--- js/calendar.js
+++ js/calendar.js
@@ -1,13 +1,15 @@
 
+let daysNames = ["Monday","Tuesday","Wensday","Thursday","Friday","Saturday","Sunday"];
+
 function displayWeek(day){
 
     let weekDays= document.getElementById("weekDays")
     let dayOfTheWeek = document.createElement("div");
     let dayName = document.createElement("p");
-    dayName.classList.add("dayName");
+    dayOfTheWeek.classList.add("dayName");
     dayName.innerHTML=day;
     dayOfTheWeek.appendChild(dayName);
     weekDays.appendChild(dayOfTheWeek);
 }
 
-displayWeek("monday");
+daysNames.forEach( (day) => displayWeek(day) );
