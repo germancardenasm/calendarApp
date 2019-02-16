1922a0cbee7d67810aab50b3093402ee2fb02736
German Cardenas <gcardenas@globalk.local>
2019-02-15 19:10:36 -0500

    months object included with all information and highligth class created to focus a day

--- js/calendar.js
+++ js/calendar.js
@@ -1,10 +1,94 @@
 
-let daysNames = ["Sunday","Monday","Tuesday","Wensday","Thursday","Friday","Saturday"];
-let maxDatOfTheMonth = 31; 
+let daysNames = ["S","M","T","W","T","F","S"];
+let months = {
+    "1": {
+      "name": "January",
+      "short": "Jan",
+      "number": 1,
+      "days": 31
+    },
+    "2": {
+      "name": "February",
+      "short": "Feb",
+      "number": 2,
+      "days": 28
+    },
+    "3": {
+      "name": "March",
+      "short": "Mar",
+      "number": 3,
+      "days": 31
+    },
+    "4": {
+      "name": "April",
+      "short": "Apr",
+      "number": 4,
+      "days": 30
+    },
+    "5": {
+      "name": "May",
+      "short": "May",
+      "number": 5,
+      "days": 31
+    },
+    "6": {
+      "name": "June",
+      "short": "Jun",
+      "number": 6,
+      "days": 30
+    },
+    "7": {
+      "name": "July",
+      "short": "Jul",
+      "number": 7,
+      "days": 31
+    },
+    "8": {
+      "name": "August",
+      "short": "Aug",
+      "number": 8,
+      "days": 31
+    },
+    "9": {
+      "name": "September",
+      "short": "Sep",
+      "number": 9,
+      "days": 30
+    },
+    "10": {
+      "name": "October",
+      "short": "Oct",
+      "number": 10,
+      "days": 31
+    },
+    "11": {
+      "name": "November",
+      "short": "Nov",
+      "number": 11,
+      "days": 30
+    },
+    "12": {
+      "name": "December",
+      "short": "Dec",
+      "number": 12,
+      "days": 31
+    }
+  }
+
+let presentDate = { 
+    day: 0,
+    monthNumber: 0,
+} 
+
+function getPresentDate(){
+    let date = new Date();
+    presentDate.day=date.getDate();
+    presentDate.monthNumber=date.getMonth()+1;
+}
 
 function writeSelectedMonth(monthName){
     let month = document.getElementById("monthName");
-    month.innerHTML=monthName;
+    month.innerHTML=months[presentDate.monthNumber].name;
 }
 
 function displayWeek(day){
@@ -21,6 +105,7 @@ function displayDates(numOfDate){
     let datesContainer= document.getElementById("datesContainer")
     let date = document.createElement("div");
     date.classList.add("date");
+    date.id=numOfDate; 
     let dateNumber= document.createElement("div");
     dateNumber.innerHTML=numOfDate;
     date.appendChild(dateNumber);
@@ -34,25 +119,36 @@ function fullFillView(){
     date.classList.add("fullFillSpace");
     datesContainer.appendChild(date);
 }
+
 function drawDates(){
-    for(let i=1; i<=maxDatOfTheMonth; i++)
-    { 
-        this.displayDates(i);
-    }
-    if(maxDatOfTheMonth%7>1)
-    {
-      let qtyOfSpacesToFullFill = 7-maxDatOfTheMonth%7;
+    for(let i=1; i<=months[presentDate.monthNumber].days; i++)
+      this.displayDates(i);
     
-      for(let i=0; i<qtyOfSpacesToFullFill; i++)
-      { 
-        this.fullFillView();
-      }
-    }       
+    if(thereIsBlankSpace())
+      drawBlankSpace()       
 }
 
+function thereIsBlankSpace(){
+    return months[presentDate.monthNumber].days%7>1
+}
 
+function drawBlankSpace(){
+    let qtyOfSpacesToFullFill = 7-months[presentDate.monthNumber].days;
+    for(let i=0; i<qtyOfSpacesToFullFill; i++)
+    { 
+      this.fullFillView();
+    }
+}
+
+function highLigthDate(dayToHighLigth){
+    let day = document.getElementById(dayToHighLigth);
+    day.classList.add("highLigth");
+}
+
+getPresentDate();
 writeSelectedMonth("January");
 daysNames.forEach( (day) => displayWeek(day));
 drawDates();
+highLigthDate(presentDate.day);
 
 
